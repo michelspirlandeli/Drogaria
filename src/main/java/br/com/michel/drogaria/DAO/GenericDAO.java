4e446c94dc3196e99f6516028a19abdb29931e65
michel spirlandeli <michelspirlandeli10@gmail.com>
2018-03-26 22:16:27 -0300

    capturar class

--- src/main/java/br/com/michel/drogaria/DAO/GenericDAO.java
+++ src/main/java/br/com/michel/drogaria/DAO/GenericDAO.java
@@ -1,11 +1,19 @@
 package br.com.michel.drogaria.DAO;
 
+import java.lang.reflect.ParameterizedType;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 import br.com.michel.drogaria.util.HibernateUtil;
 
 public class GenericDAO<Entity> {
+	
+	private Class<Entity> classe;
+	
+	@SuppressWarnings("unchecked")
+	public GenericDAO() {
+		this.classe = (Class<Entity>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
+	}
 	public void salvar(Entity entity) {
 		Session sessao = HibernateUtil.getFabricaDeSessoes().openSession();
 		Transaction transacao = null;
