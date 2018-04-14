c567cf1a68f5a4b29572bebc35742a8b7aaac06f
michel spirlandeli <michelspirlandeli10@gmail.com>
2018-04-14 16:29:05 -0300

    CidadeDAO - Editar

--- src/test/java/br/com/michel/drogaria/dao/CidadeDAOTest.java
+++ src/test/java/br/com/michel/drogaria/dao/CidadeDAOTest.java
@@ -15,23 +15,23 @@ public class CidadeDAOTest {
 	@Ignore
 	public void salvar() {
 		EstadoDAO estadoDAO = new EstadoDAO();
-		
+
 		Estado estado = estadoDAO.buscar(1L);
-		
+
 		Cidade cidade = new Cidade();
 		cidade.setNome("santa catarina");
 		cidade.setEstado(estado);
-		
+
 		CidadeDAO cidadeDAO = new CidadeDAO();
 		cidadeDAO.salvar(cidade);
 	}
-	
+
 	@Test
 	@Ignore
 	public void listar() {
 		CidadeDAO cidadeDAO = new CidadeDAO();
 		List<Cidade> resultado = cidadeDAO.listar();
-		
+
 		for (Cidade cidade : resultado) {
 			System.out.println("Código da Cidade: " + cidade.getCodigo());
 			System.out.println("Nome da Cidade: " + cidade.getNome());
@@ -41,31 +41,33 @@ public class CidadeDAOTest {
 			System.out.println();
 		}
 	}
-	
+
 	@Test
 	@Ignore
 	public void buscar() {
 		Long codigo = 3L;
-		
+
 		CidadeDAO cidadeDAO = new CidadeDAO();
 		Cidade cidade = cidadeDAO.buscar(codigo);
-		
+
 		System.out.println("Código da Cidade: " + cidade.getCodigo());
 		System.out.println("Nome da Cidade: " + cidade.getNome());
 		System.out.println("Código do Estado: " + cidade.getEstado().getCodigo());
 		System.out.println("Sigla do Estado: " + cidade.getEstado().getSigla());
 		System.out.println("Nome do Estado: " + cidade.getEstado().getNome());
 		System.out.println();
-		
+
 	}
-	
+
 	@Test
+	@Ignore
 	public void excluir() {
-Long codigo = 3L;
-		
+		Long codigo = 3L;
+	
+
 		CidadeDAO cidadeDAO = new CidadeDAO();
 		Cidade cidade = cidadeDAO.buscar(codigo);
-		
+
 		cidadeDAO.excluir(cidade);
 		System.out.println("Cidade Removida");
 		System.out.println("Código da Cidade: " + cidade.getCodigo());
@@ -74,6 +76,35 @@ Long codigo = 3L;
 		System.out.println("Sigla do Estado: " + cidade.getEstado().getSigla());
 		System.out.println("Nome do Estado: " + cidade.getEstado().getNome());
 		System.out.println();
+
+	}
+
+	@Test
+	public void editar() {
+		Long codigoCidade = 2L;
+		Long codigoEstado = 4L;
+		
+		EstadoDAO estadoDAO = new EstadoDAO();
+		Estado estado = estadoDAO.buscar(codigoEstado);
+		
+		System.out.println("Código da Estado: " + estado.getCodigo());
+		System.out.println("Sigla do Estado: " + estado.getSigla());
+		System.out.println("Nome do Estado: " + estado.getNome());
+
+		CidadeDAO cidadeDAO = new CidadeDAO();
+		Cidade cidade = cidadeDAO.buscar(codigoCidade);
+		
+		System.out.println("Cidade Editada");
+		System.out.println("Código da Cidade: " + cidade.getCodigo());
+		System.out.println("Nome da Cidade: " + cidade.getNome());
+		System.out.println("Código do Estado: " + cidade.getEstado().getCodigo());
+		System.out.println("Sigla do Estado: " + cidade.getEstado().getSigla());
+		System.out.println("Nome do Estado: " + cidade.getEstado().getNome());
+		
+		cidade.setNome("Guarapuava");
+		cidade.setEstado(estado);
+		
+		cidadeDAO.editar(cidade);
 		
 	}
 }
